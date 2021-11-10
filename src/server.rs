27b13107619861941bb91ea0e84893af02b3708f
aptgetinfo <deepak.2002.02.16@gmail.com>
2021-11-10 21:24:30 +0530

    try from

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,6 @@
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 use std::io::Read;
 use std::net::TcpListener;
 pub struct Server {
@@ -18,11 +21,15 @@ impl Server {
                     match stream.read(&mut buffer){
                         Ok(_)=>{
                             println!("Recived a request: {}",String::from_utf8_lossy(&buffer));
+                            match Request::try_from(&buffer[..]){
+                                Ok(request) => {},
+                                Err(e) => println!("Failed to parse a request: {}",e),
+                            }
                         }
                         Err(e) => println!("Failed to read from connection: {}",e),
                     }
                 }
-                Err(e) => println!("Failed to establish a connection {}",e),
+                Err(e) => println!("Failed to establish a connection: {}",e),
             }
         }
     }
