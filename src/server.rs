5d8f2a00532e6682d768fa5a3717d758e7dc3a04
aptgetinfo <deepak.2002.02.16@gmail.com>
2021-11-10 19:04:50 +0530

    loop for listening init

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,4 @@
+use std::io::Read;
 use std::net::TcpListener;
 pub struct Server {
     addr: String,
@@ -12,10 +13,16 @@ impl Server {
         let listner=TcpListener::bind(&self.addr).unwrap();
         loop {
             match listner.accept() {
-                OK((stream,_)) =>{
-                    println!("OK");
+                Ok((mut stream,_)) =>{
+                    let mut buffer=[0;1024];
+                    match stream.read(&mut buffer){
+                        Ok(_)=>{
+                            println!("Recived a request: {}",String::from_utf8_lossy(&buffer));
+                        }
+                        Err(e) => println!("Failed to read from connection: {}",e),
+                    }
                 }
-                Err(e) => println("Failed to establish a connection {}",e);
+                Err(e) => println!("Failed to establish a connection {}",e),
             }
         }
     }
