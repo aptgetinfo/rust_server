61f896ffe3bca946590cafe9b6d2c067b5031344
aptgetinfo <deepak.2002.02.16@gmail.com>
2021-11-11 18:24:45 +0530

    Lifetime started

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,5 +1,13 @@
 pub use method::Method;
 pub use request::Request;
+pub use query_string::{QueryString, Value as QueryStringValue};
+pub use request::ParseError;
 
 pub mod method;
 pub mod request;
+pub mod query_string;
+
+
+
+
+
