b79bec596b927c1342961d534adee31074bbe8ee
aptgetinfo <deepak.2002.02.16@gmail.com>
2021-11-12 17:48:00 +0530

    almost done

--- src/http/method.rs
+++ src/http/method.rs
@@ -1,5 +1,6 @@
-use::std::str::FromStr;
+use std::str::FromStr;
 
+#[derive(Debug)]
 pub enum Method {
     GET,
     DELETE,
@@ -17,18 +18,18 @@ impl FromStr for Method {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "GET"=>Ok(Self::GET),
-            "DELETE"=>Ok(Self::DELETE),
-            "POST"=>Ok(Self::POST),
-            "PUT"=>Ok(Self::PUT),
-            "HEAD"=>Ok(Self::HEAD),
-            "CONNECT"=>Ok(Self::CONNECT),
-            "OPTIONS"=>Ok(Self::OPTIONS),
-            "TRACE"=>Ok(Self::TRACE),
-            "PATCH"=>Ok(Self::PATCH),
-            _=>Err(MethodError),
+            "GET" => Ok(Self::GET),
+            "DELETE" => Ok(Self::DELETE),
+            "POST" => Ok(Self::POST),
+            "PUT" => Ok(Self::PUT),
+            "HEAD" => Ok(Self::HEAD),
+            "CONNECT" => Ok(Self::CONNECT),
+            "OPTIONS" => Ok(Self::OPTIONS),
+            "TRACE" => Ok(Self::TRACE),
+            "PATCH" => Ok(Self::PATCH),
+            _ => Err(MethodError),
         }
     }
-    }
+}
 
 pub struct MethodError;
