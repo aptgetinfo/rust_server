127835dcd2df136580612e5eb8e728a570810fa4
Deepak Kumar <64689636+aptgetinfo@users.noreply.github.com>
2021-11-13 15:54:00 +0530

    Update server.rs

--- src/server.rs
+++ src/server.rs
@@ -7,7 +7,7 @@ pub trait Handler {
     fn handle_request(&mut self, request: &Request) -> Response;
 
     fn handle_bad_request(&mut self, e: &ParseError) -> Response {
-        println!("Failed to parse request: {}", e);
+        println!("Failed to pare request: {}", e);
         Response::new(StatusCode::BadRequest, None)
     }
 }
